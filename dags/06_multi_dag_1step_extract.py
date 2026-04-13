ca7b66eef47d273b7968f8dd596063c02d38c4c6
masondev1024 <mason.dev.1024@gmail.com>
2026-04-13 22:11:23 +0900

    error fixed

--- dags/06_multi_dag_1step_extract.py
+++ dags/06_multi_dag_1step_extract.py
@@ -75,7 +75,7 @@ with DAG(
         # 전달할 데이터 -> xcom을 통해서 획득 가능(동일 dag에 존재 -> jinja 템플릿 활용)
         conf={
             # f"{DATA_PATH}/sensor_data_{{ ds_nodash }}.json"  # 여기서 전달
-            " json_path": "{{ task_instance.xcom_pull(task_ids='extract')}}"
+            "json_path": "{{ task_instance.xcom_pull(task_ids='extract')}}"
         },
 
         # dag 수행시간 세팅 => 동일하게 맞추겠다. PythonOperator의 작동시간과 (컨셉)
