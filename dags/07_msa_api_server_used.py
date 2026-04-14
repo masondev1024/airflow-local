e034214fd3603432101236b39868cc19d4171b61
masondev1024 <masondev1024@gmail.com>
2026-04-14 17:01:51 +0900

    file edited

--- dags/07_msa_api_server_used.py
+++ dags/07_msa_api_server_used.py
@@ -4,84 +4,67 @@
 # 1. 모듈 가져오기
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.operators.python import PythonOperator
 import logging
 import json
 import requests # api 호출용, MSA 서비스 호출용
-import pandas as pd
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 
 # 2. API 서버 주소
-#  API_URL = 'http://127.0.0.1:8000/predict' # 현 코드가 작동중인 컨테이너 의미
-API_URL = 'http://ai-api-server:8000/predict' # AI 서비스를 
+#    특정 컨테이너의 서비스명으로 URL을 조정 -> 해당 컨테이너로 요청 전달
+#API_URL = 'http://127.0.0.1:8000/predict' # 현 코드가 작동중인 컨테이너 의미
+API_URL = 'http://ai-api-server:8000/predict' # AI 서비스를 제공하는 컨테이너의 서비스명
 
 # 4-4. 콜백함수 정의
 def _create_dummy_data(**kwargs):
     # 차후 버전은 db 테이블에서 조회 -> 데이터 구성
-    # 현재 버전은 더미 데이터를 임시 구성 xcom 전달하여 nex task에서 사용
-    users= [
-        {"user_id": "C001", "income" : 5000, "loan_amt": 4000},
-        {"user_id": "C002", "income" : 40000, "loan_amt": 1000},
-        {"user_id": "C003", "income" : 3000, "loan_amt": 5000}
+    # 현재 버전은 더미 데이터를 임시 구성 xcom 전달  하여 다음 task에서 사용
+    users = [
+        {"user_id":"C001", "income":5000, "loan_amt":2000},
+        {"user_id":"C002", "income":4000, "loan_amt":5000},
+        {"user_id":"C003", "income":8000, "loan_amt":1000}
     ]
     # xcom으로 전달
     return users
 
 def _api_service_call(**kwargs):
-    # xcom에 게시될 때는 키값이 카멜표기법으로 조정, 추출할때는 다시 스네이크 표기법으로 복원됨
-    # 1. 이전 task의 결과물 획득 (차후 -> 데이터레이크(s3), athena, redshift, opensearch(elasticsearch의 아마존 버전) 등 서비스통해서 획득)
-    ti = kwargs['ti']
-    users_data = ti.xcom_pull(task_ids = 'task_create_dummy_data')
-    if not users_data:
-        raise ValueError(f"추출된 고객 데이터가 없습니다.")
-
-    # 2. 신용 평가 요청 및 응답 -> api 호출 (차후 LLM모델과 연계 가능) -> 통신 -> I/O -> 예외처리
+    # xcom에 게시될때는 키값이 카멜표기법으로 조정, 추출할때는 다시 스네이크표기법 복원됨
+    # 1. 이전 task의 결과물 획득 
+    #    (차후 -> 데이터레이크(s3), athena, redshift, opensearch(엘라스틱서치 aws버전),..등 서비스 통해서 획득)
+    ti         = kwargs['ti']
+    users_data = ti.xcom_pull(task_ids='task_create_dummy_data')
+    logging.info(f'요청시 전달 데이터 {users_data}')
+    # 2. 신용 평가 요청 및 응답 -> api 호출 (차후 LLM 모델과 연계 가능) -> 통신 -> I/O -> 예외처리
     try:
-        # 3. post 방식 요청, dict 형태 데이터 첨부, json 형태로 전달 (내부적으로는 객체 직렬화 처리됨)
-        res =requests.post(API_URL, json=users_data)
+        # 3.post 방식 요청, dict 형태 데이터 첨부 -> json 형태로 전달 (내부적으로는 객체 직렬화 처리됨)
+        res = requests.post( API_URL, json=users_data )
         # 실제 서비스에서는 보안 이슈로 인증 정보, 각종 키등을 헤더에 세팅해야 함
-        # 4. 요청이 성공하면 다음으로 진행 -> 200 응답코드 => skip
-        # if res.raise_fir_status() ==
-        # 5. 결과 획득 (객체의 역직렬화: json -> dict or list[dict,...])
+        # 4. 요쳥이 성공하면 다음으로 진행 => 200 응답코드 => 스킵
+        # if res.raise_for_status() == 200
+        # 5. 결과 획득 (객체의 역직렬화 : json 형태 문자열 => dict 혹은 list[ dict,.. ] 형태)
         results = res.json()
-        # 6. 결과 출력
-        logging.info(f'신용 평가 결과 획득 {results}')
+        # 6. 결과 로그 출력 
+        logging.info( f'신용 평가 결과 획득 {results}')
+        # 7. xcom 전달
         return results
-
     except Exception as e:
-        logging.info(f'API 호출 실패{e}')
+        logging.error(f'API 호출 실패 {e}')
         raise
-    pass
-def _load_users_credit(**kwargs):
-    # 1. ti(Task Instance) 객체 획득
-    ti = kwargs['ti']
-    # 2. XCom을 통해 API 호출 결과 획득
-    # PythonOperator의 return 값은 기본적으로 'return_value'라는 키로 저장됩니다.
-    uesrs_grade = ti.xcom_pull(task_ids='task_api_service_call', key='return_value')
-    if not uesrs_grade:
-        logging.error('신용 평가 결과 없음')
-        raise ValueError('신용 평가 결과 없음')
-    df = pd.DataFrame(uesrs_grade)
-    # 3. 테이블이 없으면 생성(임시편성) -> 추후 사전 작업으로 이동
-    '''
-        CREATE TABLE IF NOT EXISTS customers (
-            user_id VARCHAR(50) PRIMARY KEY,
-            income INT DEFAULT NULL,
-            loan_amt INT DEFAULT NULL,
-            credit_score INT DEFAULT NULL,
-            grade VARCHAR(10) DEFAULT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    ''' 
-    mysql_hook = MySqlHook(mysql_conn_id='mysql_default', key='return_value')
+
     
-    # conn       = mysql_hook.get_conn()
-    with mysql_hook.get_conn() as conn: 
-    # 커넥션 획득 -> 입출력 연산 영향 있음(예외처리, with문 염두)
-    # (with문을 통해 open,close를 활용하지 않았다면 마지막에 무조건 닫는 코드 존재)
-    # 7. 전체를 try ~ except로 감싸기(I/O)
-        with conn.cursor() as cursor:        
-            # 4-1. insert 구문 사용
+def _load_users_credit(**kwargs):
+    # 1. 신용 평가 결과값 획득
+    ti          = kwargs['ti']
+    users_grade = ti.xcom_pull(task_ids='task_api_service_call')
+    if not users_grade:
+        logging.error('신용 평가 결과 없음')
+        raise ValueError('신용 평가 결과 없음') # 작업 실패로 표현 -> red 태그 구성
+    
+    # 2. MySqlHook을 이용하여 연결
+    mysql_hook = MySqlHook(mysql_conn_id='mysql_default')
+    with mysql_hook.get_conn() as conn:        
+        with conn.cursor() as cursor:
+            # 3. 테이블이 없으면 생성(임시편성) -> 추후 사전 작업으로 이동
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS customers (
                     user_id VARCHAR(50) PRIMARY KEY,
@@ -91,49 +74,28 @@ def _load_users_credit(**kwargs):
                     grade VARCHAR(10) DEFAULT NULL,
                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 )
-                ''')
-            # 4. 신용 평가 별과 삽입( 추후 고객 정보 업데이트로 조정)
+            ''')
+            # 4. 신용평가 별과 삽입(추후 고객 정보 업데이트로 조정)
             sql = '''
                 insert into customers
                 (user_id, credit_score, grade)
                 values
-                (%s,%s,%s)
+                (%s, %s, %s)
             '''
             params = [
-                (data['user_id'],data['credit_score'],data['grade'])
-                for data in uesrs_grade
+                ( data['user_id'], data['credit_score'], data['grade'])
+                for data in users_grade
             ]
-            cursor.executemany(sql,params)
+            cursor.executemany( sql, params )
+            # 5. 커밋
             conn.commit()
-
-    # 4. 신용평가 결과 삽입(추후 고객 정보 업데이트로 조정)
-    #    cursor.executemany()
-    # 5. 커밋
-    # 6. 연결종료
-
-    # # 3. 데이터 검증 (방어적 코딩)
-    # if not credit_results:
-    #     logging.warning("적재할 신용 평가 데이터가 없습니다.")
-    #     return
-
-    # logging.info(f"최종 적재 단계 진입: 총 {len(credit_results)}건의 데이터를 처리합니다.")
-
-    # # 4. 데이터 순회 및 적재 (실무에서는 여기서 MySqlHook 등을 사용해 DB 업데이트)
-    # for res in credit_results:
-    #     u_id = res.get('user_id')
-    #     score = res.get('credit_score')
-    #     grade = res.get('grade')
-        
-    #     # 로그 출력으로 적재 시뮬레이션
-    #     logging.info(f"[DB LOAD] 사용자: {u_id} | 점수: {score} | 등급: {grade}")
-        
-    # # 성공 시 리턴 (필요시 다음 DAG로 경로 전달 등을 위해 사용)
-    # return True
+            # 6. 연결종료 -> 커서 닫기 -> 커넥션 닫기 : 자동
+            pass
 
 # 3. DAG 정의
 with DAG(
     dag_id      = "07_msa_api_server_used", 
-    description = "MSA 아키텍처상에 특정 서비스(ai 서빙 컨셉)를 호출하여 신용평가 수행하는 스케줄링",
+    description = "msa 상에 특정 서비스(ai 서빙 컨셉)를 호출하여 신용평가 수행하는 스케줄링",
     default_args= {
         'owner'             : 'de_2team_manager',        
         'retries'           : 1,
@@ -145,24 +107,23 @@ with DAG(
     tags        = ['msa', 'fastapi'],
 ) as dag:
     # 4. Task 정의
-
-    # 4-1. 더미 데이터 준비 -> 추후 고객 정보 저장 -> s3 업로드까지 
+    # 4-1. 더미 데이터 준비 -> 추후 고객 정보 저장 -> 추후 s3 업로드
     task_create_dummy_data = PythonOperator(
-        task_id="task_create_dummy_data",
-        python_callable= _create_dummy_data
+        task_id = "task_create_dummy_data",
+        python_callable = _create_dummy_data
     )
-
-    # 4-2. API 호출(AI 서비스 활용) -> 신용평가획득
-    task_api_service_call = PythonOperator(
-        task_id="task_api_service_call",
-        python_callable= _api_service_call
+    
+    # 4-2. API 호출(AI 서비스 활용)-> 신용평가획득
+    task_api_service_call  = PythonOperator(
+        task_id = "task_api_service_call",
+        python_callable = _api_service_call
     )
+    
     # 4-3. 결과저장 -> 추후 고객 정보 업데이트
     task_load_users_credit = PythonOperator(
-        task_id="task_load_users_credit",
-        python_callable=_load_users_credit
+        task_id = "task_load_users_credit",
+        python_callable = _load_users_credit
     )
 
-
-    # 5. 의존성, 각 task는 XCom 통신으로 데이터 공유
+    # 5. 의존성, 각 task는 xCom 통신으로 데이터 공유
     task_create_dummy_data >> task_api_service_call >> task_load_users_credit
--- dags/07_msa_api_server_used_adv.py
+++ dags/07_msa_api_server_used_adv.py
@@ -13,18 +13,21 @@ import json
 import requests # api 호출용, MSA 서비스 호출용
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 import random
+
 # 2. API 서버 주소
 #    특정 컨테이너의 서비스명으로 URL을 조정 -> 해당 컨테이너로 요청 전달
 #API_URL = 'http://127.0.0.1:8000/predict' # 현 코드가 작동중인 컨테이너 의미
 API_URL = 'http://ai-api-server:8000/predict' # AI 서비스를 제공하는 컨테이너의 서비스명
 
-
-
+# 4-4. 콜백함수 정의
 def _create_dummy_data(**kwargs):
-    # 차후 버전은 db 테이블에서 조회 -> 데이터 구성
-    # 현재 버전은 더미 데이터를 임시 구성 xcom 전달하여 nex task에서 사용
+    # 더미 데이터를 랜덤하게 구성하여 db에 입력
+    # 차후 프로젝트 구성 -> law 데이터가 어디에 저장되는지(1차 최종 위치 결정, 발생빈도, 형태)
+    # ->도메인 영향(이커머스,게임,금융,IOT,스마트팩토리,..)
+    # DB에서 고객 정보 획득 -> 더미 데이터도 DB에 입력 (단, 신용평가는 누락한 데이터)
+    # 2. MySqlHook을 이용하여 연결
     mysql_hook = MySqlHook(mysql_conn_id='mysql_default')
-    with mysql_hook.get_conn() as conn:  
+    with mysql_hook.get_conn() as conn:        
         with conn.cursor() as cursor:
             # 3. 테이블이 없으면 생성(임시편성) -> 추후 사전 작업으로 이동
             cursor.execute('''
@@ -37,8 +40,10 @@ def _create_dummy_data(**kwargs):
                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 )
             ''')
-            # 편의상 고객 ID가 중복되어서 오류가 발생하는 문제 해결하기위해 테이블 비우기
+            # 4. 편의상 고객 ID가 중복되어서 오류가 발생하는 문제 -> 기존 데이터 삭제 전략사용
+            #    여러번 테스트 할수 있으므로, 임시 편성
             cursor.execute('truncate table customers;')
+
             # 4. 신용평가 별과 삽입(추후 고객 정보 업데이트로 조정)
             sql = '''
                 insert into customers
@@ -47,30 +52,47 @@ def _create_dummy_data(**kwargs):
                 (%s, %s, %s)
             '''
             params = [
-                (
-                    f'C{i:03d}', # 고객 아이디, C001 ~ C050
-                    random.randint(3000,10000), # income
-                    random.randint(1000,5000) # loan_amt
-                    
+                ( 
+                    f'C{i:03d}',   # 고객 아이디, C001 ~ C050
+                    random.randint(3000, 10000), # 소득
+                    random.randint(1000, 5000),  # 대출, 론 총량
                 )
-                for i in range(1,51)
+                for i in range(1, 51)
             ]
             cursor.executemany( sql, params )
             # 5. 커밋
-            conn.commit()
-            # 6. 연결종료 -> 커서 닫기 -> 커넥션 닫기 : 자동
+            conn.commit()            
             pass
 
 
 def _extract_data(**kwargs):
-    pass
+    # 현재는 mysql에서 조회하여 획득
+    # 차후는 데이터의 위치에 따라 => Athena, opensearch, redshift, s3,.... 조회 => 획득
+    # DB -> MySqlHook->Pandas -> List[ dict, dict, .. ]
+    mysql_hook = MySqlHook(mysql_conn_id='mysql_default')
+    # sql -> df 구성
+    # 신용평가 점수가 없는 고객만 대상(향후, 갱신기간이 도래한 고객까지 포함)
+    df = mysql_hook.get_pandas_df('''
+        select 
+            user_id, income, loan_amt 
+        from 
+            customers
+        where
+            credit_score is NULL
+    ''')
+    # 결과셋 체크 
+    if df.empty:
+        logging.info('신규 고객 없다')
+        return []
+    # 변환(df->list[dict, dict,..]) -> xcom 전달
+    return df.to_dict(orient='records')
 
 def _api_service_call(**kwargs):
     # xcom에 게시될때는 키값이 카멜표기법으로 조정, 추출할때는 다시 스네이크표기법 복원됨
     # 1. 이전 task의 결과물 획득 
     #    (차후 -> 데이터레이크(s3), athena, redshift, opensearch(엘라스틱서치 aws버전),..등 서비스 통해서 획득)
     ti         = kwargs['ti']
-    users_data = ti.xcom_pull(task_ids='task_create_dummy_data')
+    users_data = ti.xcom_pull(task_ids='task_extract_data')
     logging.info(f'요청시 전달 데이터 {users_data}')
     # 2. 신용 평가 요청 및 응답 -> api 호출 (차후 LLM 모델과 연계 가능) -> 통신 -> I/O -> 예외처리
     try:
@@ -97,30 +119,20 @@ def _load_users_credit(**kwargs):
         logging.error('신용 평가 결과 없음')
         raise ValueError('신용 평가 결과 없음') # 작업 실패로 표현 -> red 태그 구성
     
+    logging.info(  users_grade )
+
     # 2. MySqlHook을 이용하여 연결
     mysql_hook = MySqlHook(mysql_conn_id='mysql_default')
     with mysql_hook.get_conn() as conn:        
-        with conn.cursor() as cursor:
-            # 3. 테이블이 없으면 생성(임시편성) -> 추후 사전 작업으로 이동
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS customers (
-                    user_id VARCHAR(50) PRIMARY KEY,
-                    income INT DEFAULT NULL,
-                    loan_amt INT DEFAULT NULL,
-                    credit_score INT DEFAULT NULL,
-                    grade VARCHAR(10) DEFAULT NULL,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                )
-            ''')
+        with conn.cursor() as cursor:            
             # 4. 신용평가 별과 삽입(추후 고객 정보 업데이트로 조정)
             sql = '''
-                insert into customers
-                (user_id, credit_score, grade)
-                values
-                (%s, %s, %s)
+                update customers
+                set credit_score=%s, grade=%s
+                where user_id=%s
             '''
             params = [
-                ( data['user_id'], data['credit_score'], data['grade'])
+                ( data['credit_score'], data['grade'], data['user_id'] )
                 for data in users_grade
             ]
             cursor.executemany( sql, params )
@@ -169,4 +181,4 @@ with DAG(
     )
 
     # 5. 의존성, 각 task는 xCom 통신으로 데이터 공유
-    task_create_dummy_data # >> task_extract_data >> task_api_service_call >> task_load_users_credit
+    task_create_dummy_data >> task_extract_data >> task_api_service_call >> task_load_users_credit
